ef04620ffbed8ce2bee0f85304ddc3bd83586645
ifatimahali <star18salfa@gmail.com>
2024-07-17 01:22:58 +0300

    f

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,199 +1,39 @@
-import React, { useState } from "react";
+import React from 'react';
+import logo from '../assets/logo-jad.png';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
+function NavBar() {
   return (
-    <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded={isMobileMenuOpen}
-              onClick={toggleMobileMenu}
-            >
-              <span className="absolute -inset-0.5"></span>
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className={`${isMobileMenuOpen ? "hidden" : "block"} h-6 w-6`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              <svg
-                className={`${isMobileMenuOpen ? "block" : "hidden"} h-6 w-6`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              />
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                  aria-current="page"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Calendar
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">View notifications</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
-            </button>
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </button>
-              </div>
-              <div
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-              >
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                >
-                  Your Profile
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          </div>
+   <div className='w-full mb-2'>
+     <nav className="bg-white h-[10vh]">
+      <div className="flex items-center ">
+        <div className="flex items-center space-x-4 mb-[0px]">
+          <Link to ="/" className="flex items-center mr-2 text-black hover:text-gray-300">
+            <img src={logo} className='w-auto h-[10vh] ' alt="Logo" />
+          </Link>
+        </div>
+       <div className='flex mr-auto'>
+       <div className="hidden md:flex items-center mr-auto gap-4 space-x-4">
+          <Link to="/login" className="py-2 px-3 text-2xl border border-[#9685CF] text-black hover:text-[#9685CF] rounded-lg transition duration-300">تسجيل دخول</Link>
+          <Link to="/signup" className="py-2 px-3 text-2xl bg-[#9685CF] text-white rounded-lg transition duration-300">تسجيل جديد</Link>
+        </div>
+       </div>
+        <div className="md:hidden flex items-center">
+          <button className="mobile-menu-button">
+            <svg className="w-6 h-6 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
         </div>
       </div>
-      {isMobileMenuOpen && (
-        <div className="sm:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            <a
-              href="#"
-              className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-              aria-current="page"
-            >
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-            >
-              Calendar
-            </a>
-          </div>
-        </div>
-      )}
+      <div className="mobile-menu hidden md:hidden bg-white">
+        <Link to="/login" className="block py-2 px-4 text-black hover:bg-gray-300 border border-[#9685CF] rounded">تسجيل دخول</Link>
+        <Link to="/signup" className="block py-2 px-4 text-white bg-[#9685CF] hover:bg-[#7d6eb0] rounded">تسجيل جديد</Link>
+      </div>
     </nav>
+   </div>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
+
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
+import img from '../assets/logo-jadw.png'
+import { Link } from 'react-router-dom';
 
 function SignUpPage() {
-  return (
-    <div>SignUpPage</div>
-  )
+  return (    
+    <div className="w-full min-h-screen flex items-center justify-center px-5 py-5 bg-black-100">
+        <div className="bg-white text-black-500  overflow-hidden w-[50vw]">
+            <div className="md:flex w-full rounded-lg border-2 border-[#9685CF] ">
+                <div className=" md:block w-[50vw] bg-[#9685CF] py-10 px-10">
+                    <Link to="/">
+                    <img src={img} className='mt-24'/>
+                    </Link>
+                </div>
+                <div className="w-full py-10 px-5 md:px-10">
+                    <div className="text-center mb-10">
+                        <h1 className="font-bold text-3xl text-[#9685CF]">قم بإدخال معلوماتك للتسجيل</h1>
+                    </div>
+                    <div>
+                        <div className="flex -mx-3">
+                            <div className="w-1/2 px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">الأسم الأول</label>
+                                <div className="flex">
+                                    <input type="text" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="Ali"/>
+                                </div>
+                            </div>
+                            <div className="w-1/2 px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">الأسم الاخير</label>
+                                <div className="flex">
+                                    <input type="text" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="Hakami"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex -mx-3">
+                            <div className="w-full px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">الإيميل</label>
+                                <div className="flex">
+                                    <input type="email" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="ali@gmail.com"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex -mx-3">
+                            <div className="w-full px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">كلمة المرور</label>
+                                <div className="flex">
+                                    <input type="password" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="************"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex -mx-3">
+                            <div className="w-full px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">رقم الهاتف</label>
+                                <div className="flex">
+                                    
+                                    <input type="tel" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="123-456-7890"/>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div className="flex -mx-3">
+                            <div className="w-full px-3 mb-5">
+                                <label htmlFor="" className="text-xs font-semibold px-1">العنوان</label>
+                                <div className="flex">
+                                    
+                                    <input type="text" className="w-full -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-black-200 outline-none focus:border-[#FFA842]" placeholder="123 Main St, City, Country"/>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="flex -mx-3">
+                            <div className="w-full px-3 mb-5">
+                                <button className="block w-full max-w-xs mx-auto bg-[#9685CF] hover:bg-[#FFA842] focus:bg-[#FFA842] text-white rounded-lg px-3 py-3 font-semibold">تسجيل</button>
+                                <p className='text-center p-3 text-black'> لديك حساب ؟ <Link to="/login">قم بتسجيل الدخول</Link></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
 }
+export default SignUpPage;
 
-export default SignUpPage
