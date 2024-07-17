23bfbb97df5c94e0c503a352c0bd568c7ac39cfa
Azizfalshammari <azizfalshammari.com>
2024-07-17 18:17:41 +0300

    updated

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -25,17 +25,17 @@ const Sidebar = () => {
                 width="48px"
                 height="48px"
                 viewBox="0 0 1024 1024"
-                class="icon"
+                className="icon"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#000000"
               >
-                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
                 <g
                   id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
 
                 <g id="SVGRepo_iconCarrier">
